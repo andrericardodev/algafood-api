5472fa905e777aca1c2a46f9bf492f2b5c497f35
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-06 18:09:13 -0300

    adiciona dados iniciais para forma de pagamento e permissões no script de importação

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,12 @@ INSERT INTO cozinha (nome) VALUES ('Indiana');
 
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Gourmet', 10.0, 1);
 INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Indian House', 7.99, 2);
+
+
+INSERT INTO forma_pagamento (descricao) VALUES ('Dinheiro');
+INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de crédito');
+INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de débito');
+INSERT INTO forma_pagamento (descricao) VALUES ('PIX');
+
+INSERT INTO permissao (nome, descricao) VALUES ('ADMIN', 'Administradores');
+INSERT INTO permissao (nome, descricao) VALUES ('USUARIO', 'Usuários');
