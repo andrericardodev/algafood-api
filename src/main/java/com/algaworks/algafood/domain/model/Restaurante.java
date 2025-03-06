d360a46ca3becbcdea4f9dbdd9d34dc22f3722a9
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-06 15:37:46 -0300

    adiciona a classe RestauranteRepositoryImpl e a classe ConsultaRestauranteMain; atualiza a classe Restaurante para incluir a relação com Cozinha

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,10 +1,13 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,7 +22,11 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(nullable = false)
     private String nome;
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
 
 }
