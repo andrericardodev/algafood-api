9dd878ae9cb76d65dfb4ee5ee827945d3bb24a93
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-06 15:37:21 -0300

    adiciona dados de restaurantes ao script de importação

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Gourmet', 10.0, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Indian House', 7.99, 2);
