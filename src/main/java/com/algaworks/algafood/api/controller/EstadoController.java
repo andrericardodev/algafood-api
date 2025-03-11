f2f11f3bb5e15f327f177cbe0190502d33b93fc7
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-11 16:08:24 -0300

    Adiciona GetMapping para buscar estado por Id

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,9 +3,8 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,17 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.listar();
+    }
+
+    @GetMapping("/{estadoId}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
+        Estado estado = estadoRepository.buscar(estadoId);
+
+        if (estado != null) {
+            return ResponseEntity.ok(estado);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
