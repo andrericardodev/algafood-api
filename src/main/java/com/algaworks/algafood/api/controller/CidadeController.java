7b6e304322797c196b94b11e1df7fe00901336d7
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-11 21:19:30 -0300

    Adiciona controlador para gerenciar cidades com métodos para listar e buscar por ID

---
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+public class CidadeController {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @GetMapping
+    public List<Cidade> listar() {
+        return cidadeRepository.listar();
+    }
+
+    @GetMapping("/{cidadeId}")
+    public ResponseEntity<Cidade> buscar(@PathVariable Long cidadeId) {
+        Cidade cidade = cidadeRepository.buscar(cidadeId);
+        if (cidade != null) {
+            return ResponseEntity.ok(cidade);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
