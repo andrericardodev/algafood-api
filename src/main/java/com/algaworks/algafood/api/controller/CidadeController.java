26e1b225589bc87ae82a8d21a44ad2f8e9f6c7b5
Andre Ricardo <andrericardo.developer@outlook.com>
2025-03-11 22:05:07 -0300

    Adiciona serviço para cadastro de cidade e método para adicionar cidade com tratamento de exceções

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.service.CadastroCidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,9 @@ public class CidadeController {
 
     @Autowired
     private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private CadastroCidadeService cadastroCidade;
 
     @GetMapping
     public List<Cidade> listar() {
@@ -30,6 +32,16 @@ public class CidadeController {
             return ResponseEntity.ok(cidade);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Cidade cidade) {
+        try {
+            cidade = cadastroCidade.salvar(cidade);
+            return ResponseEntity.ok(cidade);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
